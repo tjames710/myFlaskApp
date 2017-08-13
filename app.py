a522056a34cfcc3aedc40ed9b87c5d7d703b84ef
DESKTOP-7E9AP26\t_jam <trunyonworks@gmail.com>
2017-08-13 19:12:19 -0400

    navbar update, and db connection, not working on windows in the current state having issues with connecting postgresql to flask

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+import psycopg2
 
 app = Flask(__name__)
 
+#Config Postgres
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ted:TJD101213@localhost/myflaskapp'
+db = SQLAlchemy(app)
 Articles = Articles()
 
 @app.route('/')
@@ -21,5 +28,42 @@ def articles():
 def article(id):
     return render_template('article.html', id = id)
 
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        #connect to db
+        conn = psycopg2.connect("dbname=myflaskapp user=postgres")
+
+        # create cursor
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        # commit to db
+        conn.commit()
+        # close connection
+        cur.close()
+        conn.close()
+
+        flash('You are now registered and can log in!', 'success')
+
+        redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
 if __name__ == '__main__':
+    app.secret_key='secret123'
     app.run(debug=True)
